5cd87187b0b0e99939a741650611cb884dd4045e
weeds <weedsyl@163.com>
2022-09-29 18:02:38 +0800

    fix

--- src/components/content/MainTabBar.vue
+++ src/components/content/MainTabBar.vue
@@ -1,23 +1,23 @@
 <template>
   <tab-bar>
     <tab-bar-item link="/home">
-      <img slot="item-icon" src="@/assets/tabbar/home.png" alt="">
-      <img slot="item-icon-active" src="@/assets/tabbar/home-active.png" alt="">
+      <img slot="item-icon" src="@/assets/img/home/home.png" alt="">
+      <img slot="item-icon-active" src="@/assets/img/home/home-active.png" alt="">
       <div slot="item-text">首页</div>
     </tab-bar-item>
     <tab-bar-item link="/category" active-color="blue">
-      <img slot="item-icon" src="@/assets/tabbar/category.png" alt="">
-      <img slot="item-icon-active" src="@/assets/tabbar/category-active.png" alt="">
+      <img slot="item-icon" src="@/assets/img/category/category.png" alt="">
+      <img slot="item-icon-active" src="@/assets/img/category/category-active.png" alt="">
       <div slot="item-text">分类</div>
     </tab-bar-item>
     <tab-bar-item link="/cart" active-color="orange">
-      <img slot="item-icon" src="@/assets/tabbar/cart.png" alt="">
-      <img slot="item-icon-active" src="@/assets/tabbar/cart-active.png" alt="">
+      <img slot="item-icon" src="@/assets/img/cart/cart.png" alt="">
+      <img slot="item-icon-active" src="@/assets/img/cart/cart-active.png" alt="">
       <div slot="item-text">购物车</div>
     </tab-bar-item>
     <tab-bar-item link="/profile">
-      <img slot="item-icon" src="@/assets/tabbar/profile.png" alt="">
-      <img slot="item-icon-active" src="@/assets/tabbar/profile-active.png" alt="">
+      <img slot="item-icon" src="@/assets/img/profile/profile.png" alt="">
+      <img slot="item-icon-active" src="@/assets/img/profile/profile-active.png" alt="">
       <div slot="item-text">我的</div>
     </tab-bar-item>
   </tab-bar>
